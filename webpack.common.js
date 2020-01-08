ee86d9e865a9d8f735e7b967856055f3b4a72d86
leo <Leiping.Zhu@gmail.com>
2020-01-08 18:04:04 +0800

    babel preset

--- webpack.common.js
+++ webpack.common.js
@@ -37,7 +37,7 @@ module.exports = {
 				use:{
 					loader:'babel-loader',
 					options:{
-						presets:['es2015','react','stage-2']
+						presets:['@babel/preset-env','@babel/preset-react']
 					}
 				}
 			},
